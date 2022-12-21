a8545a73a09c4d177714b21324b70ffa4a672429
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-12-21 14:32:02 +0100

    contact section done, now on footer section

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -10,13 +10,21 @@ const Contacts = () => {
         <h2 className="pt-10 text-white">Get in Touch</h2>
         <h1 className="text-[var(--primary)] font-dancing mt-4">Contact Me</h1>
       </div>
-      <div className="bg-black text-white px-6 sm:px-14 py-12 grid lg:grid-cols-[30%_58%] gap-[2rem] lg:gap-[12%]">
-        <div className="flex flex-col ">
+      <div className="bg-black text-white px-6 sm:px-14 py-12 lg:grid lg:grid-cols-[30%_58%] gap-[2rem] lg:gap-[12%]">
+        <div className="flex flex-col order-2 lg:order-1">
           <div className="flex items-center gap-4 leading-[5]">
-            <MdOutlineEmail className="text-[2rem]" />
+            <AiOutlineGithub className="text-[2.5rem]" />
 
-            <a href="mailto:gbemisoyeadejumoke@gmail.com" target="_blank">
-              gbemisoyeadejumoke@gmail.com
+            <a href="" target="_blank">
+              Github
+            </a>
+          </div>
+
+          <div className="flex items-center gap-4 leading-[5]">
+            <BsLinkedin className="text-[1.8rem]" />
+
+            <a href="" target="_blank">
+              LinkedIn
             </a>
           </div>
           <div className="flex items-center gap-4 leading-[5]">
@@ -29,22 +37,19 @@ const Contacts = () => {
               Send a message
             </a>
           </div>
-          <div className="flex items-center gap-4 leading-[5]">
-            <BsLinkedin className="text-[1.8rem]" />
 
-            <a href="" target="_blank">
-              LinkedIn
-            </a>
-          </div>
           <div className="flex items-center gap-4 leading-[5]">
-            <AiOutlineGithub className="text-[2.5rem]" />
+            <MdOutlineEmail className="text-[2rem]" />
 
-            <a href="" target="_blank">
-              Github
+            <a href="mailto:gbemisoyeadejumoke@gmail.com" target="_blank">
+              gbemisoyeadejumoke@gmail.com
             </a>
           </div>
         </div>
-        <form action="" className="flex flex-col gap-5">
+        <form
+          action=""
+          className="flex flex-col gap-5 order-1 pt-8 lg:pt-0 lg:order-2"
+        >
           <input
             type="text"
             name="name"
@@ -66,7 +71,9 @@ const Contacts = () => {
             required
             className="inputs"
           ></textarea>
-          <button type="submit">Send Message</button>
+          <button type="submit" className="active">
+            Send Message
+          </button>
         </form>
       </div>
     </>
